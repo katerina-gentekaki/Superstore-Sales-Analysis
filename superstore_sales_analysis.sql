cc815b0f6830dde0d510e485f44ca95fbdfbf218
katerina-gentekaki <75811151+katerina-gentekaki@users.noreply.github.com>
2025-07-29 12:49:19 +0300

    Add files via upload

---
+++ superstore_sales_analysis.sql
@@ -0,0 +1,73 @@
+/*Business Question 1 – Total Sales by Region 
+What is the total sales amount for each region?*/
+
+SELECT SUM(sales) AS total_sales, region
+FROM sales_analysis
+GROUP BY region
+ORDER BY total_sales DESC;
+
+/*Business Question 2 – Total Sales by State
+What is the total sales amount for each region?*/
+
+SELECT SUM(sales) as total_sales, state 
+FROM sales_analysis
+GROuP BY state
+ORDER BY total_sales DESC;
+
+/*Business Question 3 – Total Profit by Product Category
+Which product categories generated the highest total profit?*/
+
+
+SELECT category, SUM(profit) AS total_profit
+FROM sales_analysis
+GROUP BY category
+ORDER BY total_profit DESC; 
+
+/*Business Question 4 – Revenue and Profit by Category
+What is the What is the total revenue, total profit, and profit margin percentage for each product category?*/
+
+SELECT 
+  category,
+  SUM(sales) AS revenue,
+  SUM(profit) AS profit,
+  ROUND(SUM(profit) / NULLIF(SUM(sales), 0) * 100, 2) AS profit_margin_percent
+FROM sales_analysis
+GROUP BY category;
+
+/*Business Question 5 – Top 5 Products by Sales
+Which products had the highest total sales?*/
+
+SELECT product_name, SUM(sales) AS total_sales
+FROM sales_analysis
+GROUP BY product_name
+ORDER BY total_sales DESC
+LIMIT 5;
+
+/*Business Question 6 – Monthly Sales Trend
+How did sales vary across different months?*/
+
+SELECT TO_CHAR(order_date, 'YYYY-MM') AS order_month, SUM(Sales) AS total_sales
+FROM sales_analysis
+GROUP BY order_month
+ORDER BY order_month DESC;
+
+/*Business Question 7 – Total Orders by Customer Segment¶
+Which customer segment placed the most orders?*/
+
+SELECT COUNT(*) as total_orders, segment
+FROM sales_analysis
+GROUP BY segment
+ORDER BY total_orders;
+
+/*Business Question 8 – Average Discount by Product Category
+What is the average discount given per product category?*/
+
+SELECT AVG(discount) as avg_discount,category 
+FROM sales_analysis
+GROUP BY category
+ORDER BY avg_discount DESC;
+
+SELECT SUM(sales) as total_sales, state 
+FROM sales_analysis
+GROUP BY state
+ORDER BY total_sales DESC;
---
+++ superstore_sales_python.py
@@ -0,0 +1,127 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns 
+import plotly.express as px
+
+df = pd.read_csv(r'C:\Users\User\Desktop\Github projects\superstore\Sample - Superstore.csv', encoding='ISO-8859-1')
+    
+print(df.head())
+df.info()
+
+# Check for missing values
+print("Missing Data:\n", df.isnull().sum())
+
+# Check the summary statistics
+print("\nSummary Statistics:\n", df.describe())
+
+# Convert Order Date and Ship Date to datetime format
+df['Order Date'] = pd.to_datetime(df['Order Date'], format='%m/%d/%Y')
+df['Ship Date']=pd.to_datetime(df['Ship Date'], format='%m/%d/%Y')
+
+# Total Sales by Region
+
+colors= ['orange', 'green', 'blue', 'red']
+df.groupby('Region')['Sales'] \
+  .sum() \
+  .plot.bar(color=colors)
+
+plt.xticks(rotation=0)
+plt.title("Top 20 Sales by Region")
+plt.xlabel("Total Sales")
+plt.ylabel("Region")
+plt.tight_layout()
+plt.show()
+
+# Top 20 states by Sales
+
+df.groupby('State')['Sales'] \
+  .sum() \
+  .sort_values(ascending=True) \
+  .tail(20) \
+  .plot.barh(color='blue')
+
+plt.title("Top 20 States by Sales")
+plt.xlabel("Total Sales")
+plt.ylabel("State")
+plt.tight_layout()
+plt.show()
+
+#Top 5 Products by Sales
+
+top5 = df.groupby('Product Name')['Sales'] \
+  .sum() \
+  .sort_values(ascending=False) \
+  .head(5) 
+
+plt.figure(figsize=(10, 6))
+plt.pie(top5, 
+        labels=top5.index, 
+        autopct='%1.1f%%',         
+        startangle=140,            
+        textprops={'fontsize': 10} 
+)
+plt.title("Top 5 Products by Sales")
+plt.tight_layout()
+plt.show()
+
+#Top 10 customers by Sales
+
+df.groupby('Customer Name')['Sales'] \
+    .sum() \
+    .sort_values(ascending=True) \
+    .tail(10) \
+    .plot.barh(color='skyblue')
+
+plt.title("Top 10 Customers by Sales")
+plt.xlabel("Total Sales")
+plt.ylabel("Customer Names")
+plt.tight_layout()
+plt.show()
+
+#Total Profit by Product Category
+
+colors= ['purple', 'green', 'blue',]
+
+df.groupby('Category')['Profit'] \
+    .sum() \
+    .sort_values(ascending=False) \
+    .plot.bar(color = colors)
+
+plt.xticks(rotation=0)
+plt.title("Total Profit by Category")
+plt.xlabel("Category")
+plt.ylabel("Total Profit")
+plt.tight_layout()
+plt.show()
+
+#Monthly Sales Trend
+
+# Create a proper datetime column for grouping
+df['Month_Year'] = df['Order Date'].dt.to_period('M').dt.to_timestamp()
+
+# Group by that month-year and sum
+monthly_sales = df.groupby('Month_Year')['Sales'].sum().reset_index()
+
+# Format the month for labeling
+monthly_sales['MonthFormatted'] = monthly_sales['Month_Year'].dt.strftime('%b %Y')
+
+#Plot
+plt.figure(figsize=(15, 6))
+plt.plot(monthly_sales['MonthFormatted'], monthly_sales['Sales'], marker='o')
+plt.xticks(rotation=45)
+plt.title("Monthly Sales Trend")
+plt.xlabel("Month")
+plt.ylabel("Sales")
+plt.tight_layout()
+plt.show()
+
+#Sales and Profit scattering by Category
+
+sns.scatterplot(data=df, x='Sales', y='Profit', hue='Category')
+plt.xlim(0, 14000)
+plt.ylim(-1000, 5000)
+plt.xlabel("Sales")
+plt.ylabel("Profit")
+plt.tight_layout()
+plt.show()
