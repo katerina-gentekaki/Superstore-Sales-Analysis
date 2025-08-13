c8f0f54ca543cf58acbeba84f9521986763a7d77
katerina-gentekaki <75811151+katerina-gentekaki@users.noreply.github.com>
2025-08-13 12:46:49 +0300

    superstore_sales_python.py

--- superstore_sales_python.py
+++ superstore_sales_python.py
@@ -1,4 +1,3 @@
-import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns 
@@ -126,3 +125,4 @@ plt.xlabel("Sales")
 plt.ylabel("Profit")
 plt.tight_layout()
 plt.show()
+
